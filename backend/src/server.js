c6ff9282f7568a0d044f6443d1a607560705abeb
mhusama <mhusama15@gmail.com>
2026-04-30 21:21:38 +0600

    Frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
 
 import express from "express";
 import notesRoutes from "./routers/notesRoutes.js";
+import {connectDB} from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("MongoDB URI from .env file:", process.env.MONGO_URI);
+const port = process.env.PORT || 5001;
 
 const app = express();
+connectDB();
 
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
 });
 
 
